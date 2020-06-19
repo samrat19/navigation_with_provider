cec0738bf5422b030a7af08665aacd6a334e02b3
Atri Das <babinsamrat123@gmail.com>
2020-06-19 14:03:45 +0530

    formats the documents

--- lib/logic/change_screen.dart
+++ lib/logic/change_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ChangeScreen extends ChangeNotifier {
-
   var currentScreen = AllScreens.screenOne;
 
-  routeScreen(String screenName){
-    if(screenName == "one"){
+  routeScreen(String screenName) {
+    if (screenName == "one") {
       currentScreen = AllScreens.screenOne;
-    }else if(screenName == "two"){
+    } else if (screenName == "two") {
       currentScreen = AllScreens.screenTwo;
-    }else{
+    } else {
       currentScreen = AllScreens.screenThree;
     }
 
