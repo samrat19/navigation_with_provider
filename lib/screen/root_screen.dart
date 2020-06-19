3ae0816f5b16253f82d2f4492fa9f4ec7e7372ab
Atri Das <babinsamrat123@gmail.com>
2020-06-19 13:20:39 +0530

    creates the root screen

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class RootScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Container();
   }
 }
